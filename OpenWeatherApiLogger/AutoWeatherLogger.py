01ed38932bb5bda2cee487268d9e384bfc918aa7
Curtylin <curtylin@gmail.com>
2020-10-01 02:21:30 -0600

    fixed most of the code, now response requests actually return a json and can put it into a log file.. silly me I overthought everything.

--- OpenWeatherApiLogger/AutoWeatherLogger.py
+++ OpenWeatherApiLogger/AutoWeatherLogger.py
@@ -4,75 +4,67 @@ import time
 import requests
 import json
 
-global currentDay, userWantsAutomaticTemperatureTracking, tempCheckRecurrance
-global lat, lon, APIKey
-
-def logWeather():
-    response = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat={0}&lon={1}&appid={2}' % str(lat), str(lon), APIKey)
-    responseObj = json.loads(response)
-    
-    temperature = responseObj.current.temp
-    pressure = responseObj.current.pressure
-    humidity = responseObj.current.humidity
-
-    print('[' + str(datetime.now()) + '] \t\t Temperature (*C): ' + str(temperature) + '\t\t Pressure (hPa): ' + str(pressure) + '\t\t Humidity (%): ' + str(humidity) + '\t\t Particulates : ' + str(particulateReading) + '\n')
-    # # writeLog.write('[' + str(datetime.now()) + '] \t\t' + str(temperature) + '\t\t' + str(pressure) + '\t\t' + str(humidity) + '\n')
-    logging.info(str(temperature) + '\t\t' + str(pressure) + '\t\t' +  str(humidity))
-
-def generateLogFile():
-    # writeLogFileName = 'AutoWeatherLog' + str(date.today()) + '.log'
-    # writeLog = open(writeLogFileName, 'w', encoding='cp1252')
-    # writeLog.write('[' + str(datetime.now()) + '] Started logging from script. \n')
-    # writeLog.write('[ Current Time ] \t\t Temperature (*C): \t\t Pressure (hPa): \t\t Humidity (%): \n')
-    logging.basicConfig(format='%(asctime)s: %(message)s', filename='AutoWeatherLog' + str(date.today()) + '.log', level=logging.INFO)
-    logging.info('Started logging from script.')
-    logging.info('Temperature (*C): \t\t Pressure (hPa): \t\t Humidity (%): \t\t Particulates: ')
-
-def userSurvey():
-    userWantsAutomaticTemperatureTracking = input("Do you want continuous temperature tracking? (Y/N): ")
-    if userWantsAutomaticTemperatureTracking.lower == 'y' or userWantsAutomaticTemperatureTracking.lower == 'yes':
-        userWantsAutomaticTemperatureTracking = True
-        try:
-            tempCheckRecurrance = float(input("How often do you want to check temperatures? (in seconds): "))
-        except:
-            tempCheckRecurrance = float(input("Please give a valid number input in seconds: "))
-        # writeLog.write('[' + str(datetime.now()) + '] Script taking info every ' + str(tempCheckRecurrance) + ' seconds. \n')
-        logging.info('Script taking info every ' + str(tempCheckRecurrance) + ' seconds.')
-    else:
-        userWantsAutomaticTemperatureTracking = False
-    # writeLog.write('[' + str(datetime.now()) + '] userWantsAutomaticTemperatureTracking: ' + str(userWantsAutomaticTemperatureTracking) + '\n')
-    logging.info('userWantsAutomaticTemperatureTracking: ' + str(userWantsAutomaticTemperatureTracking))
-    APIKey = str(input("Please give API Key for openWeatherMap: "))
-    logging.info('User Provided API Key: ' + str(APIKey))
-
-
+global currentDay, userWantsAutomaticTemperatureTracking
+global lat, lon
 
 # Inital setup for the script
 userWantsAutomaticTemperatureTracking = True
-tempCheckRecurrance = 60
+tempCheckFrequency = 60
 currentDay = str(date.today())
-# bus = SMBus(1)
-# bme280 = BME280(i2c_dev=bus)
-# pms5003 = PMS5003()
 
-APIKey = ''
+def logWeather():
+    URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +str(lat) + '&lon=' + str(lon) +  '&appid=' + APIKey + '&units=metric'
+    response = requests.get(URL)
+    responseObj = response.json()
+    
+    temperature = responseObj["current"]["temp"]
+    pressure = responseObj["current"]["pressure"]
+    humidity = responseObj["current"]["humidity"]
+
+    print('[' + str(datetime.now()) + '] \tTemperature (*C): ' + str(temperature) + '\tPressure (hPa): ' + str(pressure) + '\tHumidity (%): ' + str(humidity) + '\n')
+    # # writeLog.write('[' + str(datetime.now()) + '] \t\t' + str(temperature) + '\t\t' + str(pressure) + '\t\t' + str(humidity) + '\n')
+    logging.info(str(temperature) + '\t\t\t\t' + str(pressure) + '\t\t\t' +  str(humidity))
+
+def generateLogFile():
+    logging.basicConfig(format='%(asctime)s: %(message)s', filename='AutoWeatherLog' + str(date.today()) + '.log', level=logging.INFO)
+    logging.info('Started logging from script.')
+    logging.info('Temperature (*C): \t Pressure (hPa): \t Humidity (%): ')
+
+def userSurvey():
+    global tempCheckFrequency, userWantsAutomaticTemperatureTracking, APIKey
+    userWantsAutomaticTemperatureTracking = input("Do you want continuous temperature tracking? (Y/N): ")
+    if userWantsAutomaticTemperatureTracking.lower() == 'y' or userWantsAutomaticTemperatureTracking.lower() == 'yes':
+        userWantsAutomaticTemperatureTracking = True
+        try:
+            tempCheckFrequency = float(input("How often do you want to check temperatures? (in seconds): "))
+        except:
+            tempCheckFrequency = float(input("Please give a valid number input in seconds: "))
+            logging.info('Script taking info every ' + str(tempCheckFrequency) + ' seconds.')
+    else:
+        userWantsAutomaticTemperatureTracking = False
+        logging.info('userWantsAutomaticTemperatureTracking: ' + str(userWantsAutomaticTemperatureTracking))
+    #APIKey = str(input("Please give API Key for openWeatherMap: "))
+    #logging.info('User Provided API Key: ' + str(APIKey))
+
+
+
+APIKey = '7474a222b6f393d616e11301d843b04d'
 generateLogFile()
 userSurvey()
+print(tempCheckFrequency)
 
-lat = 40.765031
-lon = -111.849385
+lat = '40.765031'
+lon = '-111.849385'
 
 if userWantsAutomaticTemperatureTracking:
     while userWantsAutomaticTemperatureTracking:
         if currentDay != str(date.today()):     #Script automatically generates a new log file at the start of a new day.
-            # writeLog.close()
             logging.FileHandler.close()
             generateLogFile()
         logWeather()
-        time.sleep(tempCheckRecurrance)
+        time.sleep(tempCheckFrequency)
 else:
     logWeather()
 
-# writeLog.close()
 logging.FileHandler.close()
 logging.shutdown()
